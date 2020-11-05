9287fa9eb806206b34e0348fc01002bbd883f5bf
HWH-2019 <huangwenhuan2019@outlook.com>
2020-11-05 23:28:51 +0800

    update 共享栈/main.cpp

--- 共享栈/main.cpp
+++ 共享栈/main.cpp
@@ -1,47 +1,74 @@
 #include<iostream>
 using namespace std;
-#define DEFAULT_SIZE 100
-#define SUCCESS 1
-#define FAILED 0
-typedef int Status;
-template<class ElemType>
-class SeqQueue {
-    protected:
-    	int front, length;// 队头指针 ，队列长度
-        int maxSize; // 队列容量 
-        ElemType* elems; // 元素存储空间
+
+template<class T>
+class ShareStack {
+private:
+	int headtop;		//前端栈顶指针
+	int tailtop;		//后端栈顶指针
+	int maxSize;		//存储空间大小
+	T* elems;			//元素存储空间
 public:
-       SeqQueue(int size = DEFAULT_SIZE);
-       virtual ~SeqQueue();
-       bool IsEmpty() const;
-       void Clear();
-       Status DelQueue(ElemType& e);
-       Status EnQueue(const ElemType e);
-										
-       void Show() {
-          for (int i = front; i < front + length; i++) {
-                                                            						cout << elems[i % maxSize] << " ";
-          }
-		}
-	void YangHuiTriangle(int N);
+	ShareStack(int size = 100);
+	virtual ~ShareStack() { delete[] elems; }
+	bool Push(int k, const T& e);
+	bool Pop(int k, T& e);
+	bool IsEmpty();
+	bool IsFull();
+	void Show(int k);
+	T& operator[](int i) { return elems[i]; }
 };
 
-template<class ElemType>
-SeqQueue<ElemType>::SeqQueue(int size) {
-	elems = new ElemType[size];
+template<class T>
+ShareStack<T>::ShareStack(int size) {
+	headtop = -1;
+	tailtop = size;
 	maxSize = size;
-	front = 0;
-	length = 0;
+	elems = new T(size);
 }
 
-template<class ElemType>
-SeqQueue<ElemType>::~SeqQueue() {
-
+template<class T>
+bool ShareStack<T>::Push(int k, const T& e) {
+	if (IsFull()) {
+		return false;
+	}
+	else {
+		if (k == 0) {
+			elems[++headtop] = e;
+			return true;
+		}
+		else {
+			elems[--tailtop] = e;
+			return true;
+		}
+	}
 }
 
-template<class ElemType>
-bool SeqQueue<ElemType>::IsEmpty() const {
-	if (length == 0) {
+template<class T>
+bool ShareStack<T>::Pop(int k, T& e) {
+	if (k == 0) {
+		if (headtop == -1) {
+			return false;
+		}
+		else {
+			e = elems[headtop--];
+			return true;
+		}
+	}
+	else {
+		if (tailtop == maxSize) {
+			return false;
+		}
+		else {
+			e = elems[tailtop++];
+			return true;
+		}
+	}
+}
+
+template<class T>
+bool ShareStack<T>::IsEmpty() {
+	if (headtop == -1 && tailtop == maxSize) {
 		return true;
 	}
 	else {
@@ -49,66 +76,45 @@ bool SeqQueue<ElemType>::IsEmpty() const {
 	}
 }
 
-template<class ElemType>
-void SeqQueue<ElemType>::Clear() {
-	front = length = 0;
-}
-
-template<class ElemType>
-Status SeqQueue<ElemType>::DelQueue(ElemType& e) {
-	if (length == 0) {
-		return FAILED;
+template<class T>
+bool ShareStack<T>::IsFull() {
+	if (headtop == tailtop - 1) {
+		return true;
 	}
 	else {
-		e = elems[front];
-		front = (front + 1) % maxSize;
-		length--;
-		return SUCCESS;
+		return false;
 	}
 }
 
-template<class ElemType>
-Status SeqQueue<ElemType>::EnQueue(const ElemType e) {
-	if (length == maxSize) {
-		return FAILED;
-	}
-	else {
-		elems[(front + length) % maxSize] = e;
-		length++;
-		return SUCCESS;
-	}
-}
-
-
-
-template<class ElemType>
-void SeqQueue<ElemType>::YangHuiTriangle(int N){
-
-	EnQueue(1);
-	EnQueue(1);
-	int temp1=0;
-	int temp2=0;
-	for(int i=1;i<N;i++){
-		DelQueue(temp1);
-		cout<<temp1<<" ";
-		EnQueue(1);
-		int k=front;
-		for(int j=k;j<=k+i-2;j++){
-			DelQueue(temp2);
-			cout<<temp2<<" ";
-			EnQueue(temp1+temp2);
-			temp1=temp2;
+template<class T>
+void ShareStack<T>::Show(int k) {
+	if (k == 0) {
+		for (int i = 0; i <= headtop; i++) {
+			cout << elems[i] << " ";
 		}
-		if(i!=1) EnQueue(1);
-		cout<<endl;
 	}
-		
+	else {
+		for (int i = maxSize - 1; i >= tailtop; i--) {
+			cout << elems[i] << " ";
+		}
+	}
 }
 
 
 int main() {
-	SeqQueue<int> queue;
-	queue.YangHuiTriangle(18);                                                     
-	system("pause");
-   return 0;
+	ShareStack<int> test(15);
+	for (int i = 0; i < 5; i++) {
+		test.Push(0, i * 10);
+	}
+	for (int i = 14; i >= 10; i--) {
+		test.Push(1, 16);
+	}
+	test.Show(0);
+	cout << endl;
+	test.Show(1);
+	int e = 1;
+	test.Pop(0, e);
+	cout << e << endl;
+
+	return 0;
 }
